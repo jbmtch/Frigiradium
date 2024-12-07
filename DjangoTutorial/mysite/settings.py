d5d7625ea06c7af94f02898eb582d4a6df9d3d15
jbmtch <jbmtch31@gmail.com>
2024-12-06 18:51:30 -0800

    Successfully configured django server, django admin and postgresql database

--- DjangoTutorial/mysite/settings.py
+++ DjangoTutorial/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'justinmitchell',
+        'NAME': 'django_tutorial',
+        'PASSWORD': '2436',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
