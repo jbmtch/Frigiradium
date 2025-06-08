53bba087245659507878a91f6a8c45848922393a
jbmtch <jbmtch31@gmail.com>
2025-06-08 00:40:18 -0700

    wrote down skeleton for UserProfile model

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    zip_code = models.CharField(max_length=5)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    household = models.ForeignKey(Household, on_delete=models.SET_NULLL, null=True, blank=True, related_name='members')
+    phone_number = models.CharField(max_length=10)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Household(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
