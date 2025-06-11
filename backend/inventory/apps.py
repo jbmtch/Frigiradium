de56f27cfde5e623e6aff8b7da9deb2413df6c38
jbmtch <jbmtch31@gmail.com>
2025-06-10 23:50:21 -0700

    Had to install pytest, and fix errors, now have two tests passing

--- backend/inventory/apps.py
+++ backend/inventory/apps.py
@@ -5,5 +5,5 @@ class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
 
-    def ready(self);
+    def ready(self):
         import inventory.signals
--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -8,13 +8,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     zip_code = models.CharField(max_length=5)
     # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    household = models.ForeignKey(Household, on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
+    household = models.ForeignKey('Household', on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
     phone_number = models.CharField(max_length=10)
     updated_at = models.DateTimeField(auto_now=True)
-
-
 
 class Household(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+
