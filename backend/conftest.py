2666e72e510b28a1c1fb2ca76582df73206575ea
jbmtch <jbmtch31@gmail.com>
2025-08-01 10:17:11 -0700

    Fixed typo that was causing second import error. now focusing on first import error

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,4 +1,8 @@
 from dotenv import load_dotenv
 import os
+import django
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../.env'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
---
+++ backend/inventory/tests/test_dummy.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+def test_django_settings():
+    assert settings.configured
