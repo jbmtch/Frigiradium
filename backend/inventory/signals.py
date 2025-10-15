d393a7e9f035b1448dc6b5d94d2e43b83fd5362b
jbmtch <jbmtch31@gmail.com>
2025-10-15 15:47:21 -0700

    updating signal so that receiver receives actual model instance instead of settings.AUTH_USER_MODEL

--- backend/inventory/signals.py
+++ backend/inventory/signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import post_save
 from django.conf import settings
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import UserProfile
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+User = get_user_model()
+
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
