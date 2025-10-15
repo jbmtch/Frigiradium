0a1ec5871870cb1c682f7d0eb9d2c22c16fcbd90
jbmtch <jbmtch31@gmail.com>
2025-10-14 22:59:23 -0700

    Added some tests for food_item just to get started

--- backend/inventory/tests/models/test_food_item_model.py
+++ backend/inventory/tests/models/test_food_item_model.py
@@ -26,4 +26,54 @@ def test_name_length_validation():
 
     assert "Ensure this value has at most 100 characters (it has 101)." in excinfo.value.message_dict["name"]
 
+@pytest.mark.django_db
+def test_name_not_blank():
+    food_item = factories.FoodItemFactory.build(name='')
+    user = factories.UserFactory()
+    inventory = factories.InventoryFactory()
+    unit = factories.MeasurementUnitFactory()
+
+    food_item.user = user
+    food_item.inventory = inventory
+    food_item.unit = unit
+    
+
+    with pytest.raises(ValidationError) as excinfo:
+        food_item.full_clean()
+
+    assert excinfo.value.message_dict['name'] == ['This field cannot be blank.']
+
+@pytest.mark.django_db
+def test_category_only_valid_choices():
+    food_item = factories.FoodItemFactory()
+    user = factories.UserFactory()
+    inventory = factories.InventoryFactory()
+    unit = factories.MeasurementUnitFactory()
+
+    food_item.user = user
+    food_item.inventory = inventory
+    food_item.unit = unit
+
+    choices = ['FRUIT', 'VEGETABLE', 'GRAIN', 'DAIRY', 'PROTEIN' , 'OTHER']
+
+    assert food_item.category in choices
+
+@pytest.mark.django_db
+def test_category_prevents_invalid_choices():
+    food_item = factories.FoodItemFactory.build(category="SNACK")
+    user = factories.UserFactory()
+    inventory = factories.InventoryFactory()
+    unit = factories.MeasurementUnitFactory()
+
+    food_item.user = user
+    food_item.inventory = inventory
+    food_item.unit = unit
+
+    choices = ['FRUIT', 'VEGETABLE', 'GRAIN', 'DAIRY', 'PROTEIN' , 'OTHER']
+
+    with pytest.raises(ValidationError) as excinfo:
+        food_item.full_clean()
+
+    assert food_item.category not in choices
+    assert excinfo.value.message_dict['category'] == ["Value 'SNACK' is not a valid choice."]
 
