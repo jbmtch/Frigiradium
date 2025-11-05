92b2d51b87c7bb8856c6711204c910375ad1e0aa
jbmtch <jbmtch31@gmail.com>
2025-11-04 22:37:26 -0800

    Added UserInventory model and user field to Household model for more ease in querysets for views, added viewset and serializer for households

--- backend/inventory/serializers.py
+++ backend/inventory/serializers.py
@@ -1,8 +1,15 @@
-from inventory.models import UserProfile
+from inventory.models import UserProfile, Household
 from rest_framework import serializers
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
+class HouseholdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Household
+        fields = '__all__'
+
+
 
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from inventory.serializers import UserProfileSerializer
+from inventory.serializers import UserProfileSerializer, HouseholdSerializer
 from rest_framework import permissions, viewsets
-from inventory.models import UserProfile
+from inventory.models import UserProfile, Household
 # Create your views here.
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     # permission_classes = [permissions.IsAuthenticated]
     queryset = UserProfile.objects.all()
+
+class HouseholdViewSet(viewsets.ModelViewSet):
+    serializer_class = HouseholdSerializer
+    queryset = Household.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        return super().get_queryset().filter(user=user)
+    
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
