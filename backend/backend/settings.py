bbc4d33b8d9b742d7ad8d1f115822632d8d74379
jbmtch <jbmtch31@gmail.com>
2026-04-05 11:12:37 -0700

    Added drf-spectacular, swaggerUI, and ReDoc via OpenAPI in order to import API routes and endpoints to postman for testing. Also added documentation for API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'inventory'
 ]
 
@@ -141,3 +142,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Frigiradium API",
+    "DESCRIPTION": "OpenAPI schema for the Frigiradium backend.",
+    "VERSION": "1.0.0",
+}
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inventory/', include("inventory.urls"))
+    path('inventory/', include("inventory.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
